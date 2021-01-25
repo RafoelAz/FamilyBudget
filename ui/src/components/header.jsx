a08e8b8a1fd25b1852cdcb51417621c4c9fe2a7e
M1chman <76171452+M1chman@users.noreply.github.com>
2021-01-25 14:26:09 +0200

    create header.jsx

---
+++ ui/src/components/header.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+  return (
+    <header className="navbar navbar-dark stiky-top bg-dark bgblack flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#/">
+        Family Budget:
+      </a>
+    </header>
+  );
+}
+
+export { Header };
