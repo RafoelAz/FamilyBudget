e434073e952b447c41576de991d0c55a23c5446f
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2021-01-30 20:41:31 +0100

    Fix eslint warnings

--- ui/src/pages/landing.page.jsx
+++ ui/src/pages/landing.page.jsx
@@ -1,6 +1,4 @@
 import './landing.page.css';
-import { Header } from '../components/header';
-import { Nav } from '../components/nav';
 
 export function LandingPage() {
   return <p>Landing page</p>;
