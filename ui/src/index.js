f43db513a373af157e270c00e5850be50df4c353
M1chman <76171452+M1chman@users.noreply.github.com>
2021-02-18 20:47:20 +0200

    include Provider from react-redux in project

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,16 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import allReducers from './redusers';
 
 const store = createStore(allReducers);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
 
