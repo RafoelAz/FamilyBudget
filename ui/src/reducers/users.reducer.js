8e3865b97119435d3a9b3569f4327d4973926c62
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2021-02-28 20:21:21 +0100

    Use initial state

--- ui/src/reducers/users.reducer.js
+++ ui/src/reducers/users.reducer.js
@@ -1,20 +1,22 @@
-export default function usersReducer() {
-  return [
-    {
-      id: 1,
-      name: 'Vadik',
-      month: 200,
-      year: 2200,
-      data: '16.02.2020',
-      total_money: 2500,
-    },
-    {
-      id: 2,
-      name: 'Ira',
-      month: 3000,
-      year: 45000,
-      data: '16.02.2020',
-      total_money: 50000,
-    },
-  ];
+const initialState = [
+  {
+    id: 1,
+    name: 'Vadik',
+    month: 200,
+    year: 2200,
+    data: '16.02.2020',
+    total_money: 2500,
+  },
+  {
+    id: 2,
+    name: 'Ira',
+    month: 3000,
+    year: 45000,
+    data: '16.02.2020',
+    total_money: 50000,
+  },
+];
+
+export default function usersReducer(state = initialState, action) {
+  return state;
 }
