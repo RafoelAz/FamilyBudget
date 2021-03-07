80cbaf3ea9c59e8e945f59b31946505868c30a27
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2021-03-07 13:02:27 +0100

    Dummy counter example

--- api/index.js
+++ api/index.js
@@ -1,9 +1,30 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import cors from 'cors';
 
-app.get('/', (req, res) => {
-  res.send('hellooooo!');
+const app = express();
+const port = 3003;
+
+app.use(cors());
+
+let count = 0;
+
+app.get('/', (request, response) => {
+  response.send(String(count));
+});
+
+app.get('/increment', (request, response) => {
+  count = count + 1;
+  setTimeout(() => {
+    response.send(String(count));
+  }, 1000);
+});
+
+app.get('/decrement', (request, response) => {
+  count = count - 1;
+
+  setTimeout(() => {
+    response.send(String(count));
+  }, 2000);
 });
 
 app.listen(port, () => {
--- ui/src/pages/dashboard.page.jsx
+++ ui/src/pages/dashboard.page.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function DashboardPage() {
-  return <h1>Dashboard Page</h1>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:3003/')
+      .then((response) => response.text())
+      .then((responseNumber) => {
+        setCount(parseInt(responseNumber));
+      });
+  }, []);
+
+  const increment = () => {
+    fetch('http://localhost:3003/increment')
+      .then((response) => response.text())
+      .then((responseNumber) => {
+        setCount(parseInt(responseNumber));
+      });
+  };
+
+  const decrement = () => {
+    fetch('http://localhost:3003/decrement')
+      .then((response) => response.text())
+      .then((responseNumber) => {
+        setCount(parseInt(responseNumber));
+      });
+  };
+
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </>
+  );
 }
